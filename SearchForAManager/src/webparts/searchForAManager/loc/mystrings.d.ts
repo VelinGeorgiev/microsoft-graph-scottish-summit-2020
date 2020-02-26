9440e6d1b9205154a5b5db64e63624a9b4daa970
Velin Georgiev <velin.georgiev@gmail.com>
2020-02-26 11:41:14 +0000

    empty js web parts added

---
+++ SearchForAManager/src/webparts/searchForAManager/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface ISearchForAManagerWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'SearchForAManagerWebPartStrings' {
+  const strings: ISearchForAManagerWebPartStrings;
+  export = strings;
+}
