5116c67d7cc2d24acf43b876a8802a3e51cc7991
Velin Georgiev <velin.georgiev@gmail.com>
2020-02-26 13:04:16 +0000

    Assign a manager added

--- AssignAManager/src/webparts/assignAManager/AssignAManagerWebPart.ts
+++ AssignAManager/src/webparts/assignAManager/AssignAManagerWebPart.ts
@@ -4,10 +4,8 @@ import {
   PropertyPaneTextField
 } from '@microsoft/sp-property-pane';
 import { BaseClientSideWebPart } from '@microsoft/sp-webpart-base';
-import { escape } from '@microsoft/sp-lodash-subset';
-
-import styles from './AssignAManagerWebPart.module.scss';
 import * as strings from 'AssignAManagerWebPartStrings';
+import { MSGraphClient } from '@microsoft/sp-http';
 
 export interface IAssignAManagerWebPartProps {
   description: string;
@@ -17,25 +15,40 @@ export default class AssignAManagerWebPart extends BaseClientSideWebPart <IAssig
 
   public render(): void {
     this.domElement.innerHTML = `
-      <div class="${ styles.assignAManager }">
-    <div class="${ styles.container }">
-      <div class="${ styles.row }">
-        <div class="${ styles.column }">
-          <span class="${ styles.title }">Welcome to SharePoint!</span>
-  <p class="${ styles.subTitle }">Customize SharePoint experiences using Web Parts.</p>
-    <p class="${ styles.description }">${escape(this.properties.description)}</p>
-      <a href="https://aka.ms/spfx" class="${ styles.button }">
-        <span class="${ styles.label }">Learn more</span>
-          </a>
-          </div>
-          </div>
-          </div>
-          </div>`;
+      <input type="input" id="employee" placeholder="Email of the employee" autocomplete="on" name="email" />
+      <input type="input" id="manager" placeholder="Email of the manager" autocomplete="on" name="email" />
+      <button id="assign">Assign</button>
+      <pre id="result"></pre>`;
+
+    const assignButton = document.getElementById('assign');
+    assignButton.addEventListener('click', () => {
+
+      const manager = document.getElementById('manager') as HTMLInputElement;
+      const employee = document.getElementById('employee') as HTMLInputElement;
+
+      this.context.msGraphClientFactory
+        .getClient()
+        .then((client: MSGraphClient) => {
+
+          client
+          .api(`/users/${encodeURIComponent(manager.value)}/manager/$ref`)
+          .header('Content-Type', 'application/json')
+          .put({ 
+                "@odata.id": `https://graph.microsoft.com/v1.0/users/${encodeURIComponent(employee.value)}` 
+               }, 
+              (err, res) => {
+
+                const result = document.getElementById('result');
+                result.innerHTML = JSON.stringify((err || res), null, 2);
+
+              });
+        });
+    });
   }
 
   protected get dataVersion(): Version {
-  return Version.parse('1.0');
-}
+    return Version.parse('1.0');
+  }
 
   protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
   return {
